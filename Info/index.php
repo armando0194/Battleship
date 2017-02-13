0dbf18cd7f4951bab6b76f2e11e684ab13e53c15
arman <arman@172.19.155.203>
2017-02-13 13:34:15 -0700

    added play

--- Info/index.php
+++ Info/index.php
@@ -25,11 +25,14 @@
 	}
 	
 	function returnInfoJson() {
+		//Create new ships
 		$aircraft = new Ship("Aircraft carrier", 5);
 		$battleship = new Ship("Battleship", 4);
 		$frigate = new Ship("Frigate", 3);
 		$submarine = new Ship("Submarine", 3);
 		$minesweeper = new Ship("Minesweeper", 2);
+		
+		//create json and return it
 		$InfoObject = new Info(10, Array("Smart", "Random", "Sweep"), Array($aircraft, $battleship, $frigate, $submarine, $minesweeper));
 		header('Content-Type: application/json');
 		echo json_encode($InfoObject);
