a932c2a3ac793c03203469ee7cb1ec8d64236a9a
arman <arman@DESKTOP-I8V1J99>
2017-02-14 13:36:53 -0700

    modified new

--- new/index.php
+++ new/index.php
@@ -4,23 +4,73 @@ checkURLParameters();
 
 function validateStrategy(){
 	$strategy =  $_GET['strategy'];
-	if(true){
-		$startegyErrorMessage = new ErrorResponse("Strategy not specified");
+	strtolower($strategy);
+	if(!$strategy){
+		$strategyErrorMessage = new ErrorResponse("Strategy not specified");
 	}
-	if($strategy == "Smart" || $strategy == "Random") {
-		
+	if($strategy != "smart" || $strategy != "random" || $strategy != "sweep") {
+		$strategyErrorMessage = new ErrorResponse("Unknown Strategy");
 	}
 	else {
 		return;
 	}
-	echo json_encode($startegyErrorMessage);
+	echo json_encode($strategyErrorMessage);
+}
+
+function validateDeployment(){
+	$ships = $_GET['ships'];
+	if(!$ships){
+		return; // gen random deployment
+	}
+	$ships = explode(";",$ships);
+	if(sizeof($ships) != 5){
+		$deploymentErrorMessage = new ErrorResponse("Ship Deployment not well formed");
+	}
+	$ships = explode(",",$ships);
+	foreach($ships as $ship){
+		if(sizeof($ship) !=4){
+			$deploymentErrorMessage = new ErrorResponse("Incomplete ship deployment");
+		}
+		list($name,$xPos,$yPos,$direction) = explode(",",$ship);
+		if(validateShipName($name)){
+			$deploymentErrorMessage = new ErrorResponse("Unknown ship name");
+		}
+		if(validateShipPosition($xPos) || validateShipPositio($YPos)){
+			$deploymentErrorMessage = new ErrorResponse("Invalid Ship Position");
+		}
+		if(validateShipDirection($xPos,$yPos,$direction)){
+			$deploymentErrorMessage = new ErrorResponse("Invalid Ship Direction");
+		}
+	}
+}
+function validateShipDirection($xPos,$yPos,$direction){
+	if($xPos == 10 && $direction){
+		return true;
+	}
+	if($yPos == 10 && direction){
+		return true;
+	}
+	return false;
+}
+function validateShipPosition($pos){
+	if($pos < 0 || $pos > 10){
+		return true;
+	}
+	return false;
+}
+function validateShipName($name){
+	strtolower($name);
+	if(!$name || $name !="battleship" ||  $name !="aircraft+carrier" ||  $name !="submarine" || $name !="frigate" ||  $name !="minesweepere"){
+		return true;
+	}
+
+	return false;
 }
 
 function checkURLParameters(){
 	echo $strategy;
-	
-	validateStrategy();
-		
-}
 
+	validateStrategy();
+
+}
 ?>
