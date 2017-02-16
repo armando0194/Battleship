88680c0ea90698725aa11deb8eda0081318f5089
Daniel Ornelas <Daniel Ornelas@DESKTOP-H39ERPR>
2017-02-16 14:28:50 -0700

    new Game correct response

--- new/index.php
+++ new/index.php
@@ -1,47 +1,69 @@
 <?php
+include "../common/Response.php";
+include "../common/Board.php";
+include "../common/GlobalFunctions.php";
 
-checkURLParameters();
+newGame();
+class newGame {
+	public $response;
+	public $pid;
+	
 
-function validateStrategy(){
+	function __construct($response){
+		$this->response = $response;
+		$this->pid = uniqid();
+	}
+	function toJson($this){
+		json_encode(array_filter((array) $this, 'is_not_null'));
+	}
+}
+function newGame(){
+	if(checkURLParameters())
+	$newGameResponse = new newGame(true);
+	$newGameResponse ->toJson($this);
+}
+
+function getStrategy(){
 	$strategy =  $_GET['strategy'];
 	strtolower($strategy);
 	if(!$strategy){
-		$strategyErrorMessage = new ErrorResponse("Strategy not specified");
+		$strategyErrorMessage = Response::withReason("Strategy not specified");
 	}
 	if($strategy != "smart" || $strategy != "random" || $strategy != "sweep") {
-		$strategyErrorMessage = new ErrorResponse("Unknown Strategy");
+		$strategyErrorMessage = Response::withReason("Unknown Strategy");
 	}
 	else {
-		return;
+		return $strategy;
 	}
-	echo json_encode($strategyErrorMessage);
+	echo json_encode(array_filter((array) $strategyErrorResponse, 'is_not_null'));
 }
 
-function validateDeployment(){
+function getDeployment(){
 	$ships = $_GET['ships'];
 	if(!$ships){
 		return; // gen random deployment
 	}
 	$ships = explode(";",$ships);
 	if(sizeof($ships) != 5){
-		$deploymentErrorMessage = new ErrorResponse("Ship Deployment not well formed");
+		$deploymentErrorMessage = Response::withReason("Ship Deployment not well formed");
 	}
 	$ships = explode(",",$ships);
 	foreach($ships as $ship){
 		if(sizeof($ship) !=4){
-			$deploymentErrorMessage = new ErrorResponse("Incomplete ship deployment");
+			$deploymentErrorMessage = Response::withReason("Incomplete ship deployment");
 		}
 		list($name,$xPos,$yPos,$direction) = explode(",",$ship);
 		if(validateShipName($name)){
-			$deploymentErrorMessage = new ErrorResponse("Unknown ship name");
+			$deploymentErrorMessage = Response::withReason("Unknown ship name");
 		}
 		if(validateShipPosition($xPos) || validateShipPositio($YPos)){
-			$deploymentErrorMessage = new ErrorResponse("Invalid Ship Position");
+			$deploymentErrorMessage = Response::withReason("Invalid Ship Position");
 		}
 		if(validateShipDirection($xPos,$yPos,$direction)){
-			$deploymentErrorMessage = new ErrorResponse("Invalid Ship Direction");
+			$deploymentErrorMessage = Response::withReason("Invalid Ship Direction");
 		}
 	}
+	
 }
 function validateShipDirection($xPos,$yPos,$direction){
 	if($xPos == 10 && $direction){
@@ -53,14 +75,14 @@ function validateShipDirection($xPos,$yPos,$direction){
 	return false;
 }
 function validateShipPosition($pos){
-	if($pos < 0 || $pos > 10){
+	if($pos < 1 || $pos > 10){
 		return true;
 	}
 	return false;
 }
 function validateShipName($name){
 	strtolower($name);
-	if(!$name || $name !="battleship" ||  $name !="aircraft+carrier" ||  $name !="submarine" || $name !="frigate" ||  $name !="minesweepere"){
+	if(!$name || $name !="battleship" ||  $name !="aircraft+carrier" ||  $name !="submarine" || $name !="frigate" ||  $name !="minesweeper"){
 		return true;
 	}
 
@@ -68,9 +90,8 @@ function validateShipName($name){
 }
 
 function checkURLParameters(){
-	echo $strategy;
-
-	validateStrategy();
+	$strategy = getStrategy();
+	$deployment = getDeployment();
 
 }
 ?>
